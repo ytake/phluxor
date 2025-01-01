60bc83d159106b5de877ee5870226542f111066c
yuuki takezawa <yuuki.takezawa@comnect.jp.net>
2025-01-02 02:22:06 +0900

    changed impl

--- src/Phluxor/ActorSystem/SliceMap.php
+++ src/Phluxor/ActorSystem/SliceMap.php
@@ -5,177 +5,80 @@ declare(strict_types=1);
 namespace Phluxor\ActorSystem;
 
 use SplFixedArray;
-use GMP;
 
 class SliceMap
 {
-    /** @var SplFixedArray<ConcurrentMap|null> */
-    private SplFixedArray $buckets;
-
-    public function __construct(int $bucketSize = 1024)
-    {
-        $this->buckets = new SplFixedArray($bucketSize);
-        for ($i = 0; $i < $bucketSize; $i++) {
-            $this->buckets[$i] = null;
+    /**
+     * @param SplFixedArray<ConcurrentMap> $fixedArray
+     */
+    public function __construct(
+        private SplFixedArray $fixedArray = new SplFixedArray(1024)
+    ) {
+        for ($i = 0; $i < $this->fixedArray->count(); $i++) {
+            $this->fixedArray[$i] = new ConcurrentMap();
         }
     }
 
     public function getBucket(string $key): ConcurrentMap
     {
-        // GMP版 MurmurHash3(32bit)
-        $hash32 = $this->murmurHash3_32_GMP($key);
-
-        // other implementation
-        // if (gmp_cmp($hash32, gmp_init('0x80000000')) >= 0) {
-        //     $index = gmp_intval(gmp_sub($hash32, gmp_init('4294967296')));
-        // } else {
-        //     $index = gmp_intval($hash32);
-        // }
-        $index = gmp_intval($hash32) % $this->buckets->count();
-        if ($index < 0) {
-            $index += $this->buckets->count();
+        $pack = unpack('C*', $key);
+        if ($pack !== false) {
+            $hash = $this->seedSum32(0, $pack);
+            $index = $hash % 1024;
+            return $this->fixedArray[$index];
         }
-
-        $bucket = $this->buckets[$index];
-        if ($bucket === null) {
-            $bucket = new ConcurrentMap();
-            $this->buckets[$index] = $bucket;
-        }
-
-        return $bucket;
+        return new ConcurrentMap();
     }
 
-    /**
-     * GMPで 32ビットのMurmurHash3を実装
-     */
-    private function murmurHash3_32_GMP(string $key): GMP
+    private function SeedSum32(int $seed, array $data): int
     {
-        $length = strlen($key);
+        $h1 = $seed;
+        $clen = count($data);
+        $c1_32 = 0xcc9e2d51;
+        $c2_32 = 0x1b873593;
 
-        // 定数をGMP化
-        $c1 = gmp_init('0xcc9e2d51', 16);
-        $c2 = gmp_init('0x1b873593', 16);
-        $five = gmp_init(5, 10);
-        $mixConst = gmp_init('0xe6546b64', 16);
+        for ($i = 1; $i + 3 <= $clen; $i += 4) {
+            $k1 = ($data[$i] | $data[$i + 1] << 8 | $data[$i + 2] << 16 | $data[$i + 3] << 24);
+            $k1 = $this->multiplyAndRotate($k1, $c1_32, 15);
+            $k1 *= $c2_32;
 
-        // h1 (seed=0)
-        $h1 = gmp_init(0, 10);
-
-        // 4バイトごと処理
-        $roundedEnd = $length & ~3; // 4の倍数に切り捨て
-        for ($i = 0; $i < $roundedEnd; $i += 4) {
-            $k1 = gmp_init(
-                (ord($key[$i]) & 0xff)
-                | ((ord($key[$i + 1]) & 0xff) << 8)
-                | ((ord($key[$i + 2]) & 0xff) << 16)
-                | ((ord($key[$i + 3]) & 0xff) << 24),
-                10
-            );
-            // k1 *= c1
-            $k1 = self::gmpAnd32(gmp_mul($k1, $c1));
-            // k1 = rotateLeft(k1, 15)
-            $k1 = self::gmpRotl32($k1, 15);
-            // k1 *= c2
-            $k1 = self::gmpAnd32(gmp_mul($k1, $c2));
-
-            // h1 ^= k1
-            $h1 = gmp_xor($h1, $k1);
-            // h1 = rotateLeft(h1, 13)
-            $h1 = self::gmpRotl32($h1, 13);
-            // h1 = h1*5 + 0xe6546b64
-            $h1 = gmp_add(self::gmpAnd32(gmp_mul($h1, $five)), $mixConst);
-            $h1 = self::gmpAnd32($h1);
+            $h1 ^= intval($k1);
+            $h1 = $this->multiplyAndRotate($h1, 5, 13);
+            $h1 += 0xe6546b64;
         }
 
-        // 残り (1~3バイト)
-        $k1 = gmp_init(0, 10);
-        $tailSize = $length & 3;
-        if ($tailSize >= 3) {
-            $k1 = gmp_xor(
-                $k1,
-                gmp_init((ord($key[$roundedEnd + 2]) & 0xff) << 16, 10)
-            );
+        $k1 = 0;
+        for ($j = $i; $j < $clen; $j++) {
+            $k1 ^= $data[$j] << ($j - $i) * 8;
         }
-        if ($tailSize >= 2) {
-            $k1 = gmp_xor(
-                $k1,
-                gmp_init((ord($key[$roundedEnd + 1]) & 0xff) << 8, 10)
-            );
-        }
-        if ($tailSize >= 1) {
-            $k1 = gmp_xor(
-                $k1,
-                gmp_init((ord($key[$roundedEnd]) & 0xff), 10)
-            );
-
-            $k1 = self::gmpAnd32(gmp_mul($k1, $c1));
-            $k1 = self::gmpRotl32($k1, 15);
-            $k1 = self::gmpAnd32(gmp_mul($k1, $c2));
-
-            $h1 = gmp_xor($h1, $k1);
+        if ($k1 !== 0) {
+            $k1 = $this->multiplyAndRotate($k1, $c1_32, 15);
+            $k1 *= $c2_32;
+            $h1 ^= (int) $k1;
         }
 
-        // lengthを XOR
-        $h1 = gmp_xor($h1, gmp_init($length, 10));
+        $h1 ^= $clen;
+        $h1 = $this->finalizeHash32($h1);
 
-        // fmix32
-        // h1 ^= h1 >> 16
-        $h1 = gmp_xor($h1, self::gmpShiftRight($h1, 16));
-        $h1 = self::gmpAnd32(gmp_mul($h1, gmp_init('0x85ebca6b', 16)));
-        $h1 = gmp_xor($h1, self::gmpShiftRight($h1, 13));
-        $h1 = self::gmpAnd32(gmp_mul($h1, gmp_init('0xc2b2ae35', 16)));
-        $h1 = gmp_xor($h1, self::gmpShiftRight($h1, 16));
-        return self::gmpAnd32($h1);
+        return $h1 & 0xffffffff;
     }
 
-    /**
-     * gmp 32bit  (val & 0xffffffff)
-     * @param GMP $val
-     * @return GMP
-     */
-    private static function gmpAnd32(GMP $val): GMP
+    private function multiplyAndRotate(int $value, int $multiplier, int $rot): int
     {
-        static $mask32 = null;
-        if (!$mask32) {
-            // 0xffffffff
-            $mask32 = gmp_init('4294967295', 10);
-        }
-        return gmp_and($val, $mask32);
+        $value *= $multiplier;
+        $value = intval($value);
+        return ($value << $rot) | ($value >> (32 - $rot));
     }
 
-    /**
-     * gmp 32bit rotate-left
-     * @param GMP $val
-     * @param int $shift
-     * @return GMP
-     */
-    private static function gmpRotl32(GMP $val, int $shift): GMP
+    private function finalizeHash32(int $h): int
     {
-        $shift = $shift & 31;
-        $val = self::gmpAnd32($val);
-        $ls = gmp_mul($val, gmp_init(2 ** $shift, 10));
-        $ls = self::gmpAnd32($ls);
-        // right-shift
-        $rsShift = 32 - $shift;
-        // $val >> (32-$shift)
-        $rs = self::gmpShiftRight($val, $rsShift);
-        // OR
-        return gmp_or($ls, $rs);
-    }
-
-    /**
-     * gmp 32bit shift-right
-     * subtract 2^$shift
-     *
-     * gmpで論理右シフト(符号を維持しない右シフト)
-     * PHPの算術シフトと違い、GMPなので自力で 2^$shift
-     * @param GMP $val
-     * @param int $shift
-     * @return GMP
-     */
-    private static function gmpShiftRight(GMP $val, int $shift): GMP
-    {
-        // $val // 2^$shift
-        return gmp_div_q($val, gmp_init(2 ** $shift, 10));
+        $h ^= $h >> 16;
+        $h *= 0x85ebca6b;
+        $h = intval($h);
+        $h ^= $h >> 13;
+        $h *= 0xc2b2ae35;
+        $h = intval($h);
+        $h ^= $h >> 16;
+        return $h;
     }
 }
